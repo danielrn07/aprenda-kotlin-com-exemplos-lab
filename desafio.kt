ebc3a003e7d331f9967162b95ea20682cde0b50d
Daniel Nascimento <79328053+danielrn07@users.noreply.github.com>
2023-01-11 22:10:38 -0300

    Conclusão do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,62 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel {
+    BASICO,
+    INTERMEDIARIO,
+    AVANCADO
+}
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    /*
+    Cria conteúdos educacionais
+    - As listas com conteúdos educacionais podem ser criadas dentro da formação, porém decidi criá-las de forma separada para organizar o código */
+    val kotlin = mutableListOf(ConteudoEducacional("Curso Kotlin", 20))
+    val javaScript = mutableListOf(ConteudoEducacional("Curso JavaScript", 30))
+    val python = mutableListOf(ConteudoEducacional("Curso Python", 60))
+
+    // Cria formações
+    val kotlinExperience = Formacao("Kotlin Experience", kotlin, Nivel.AVANCADO)
+    val javaScriptExperience = Formacao("JavaScript Experience", javaScript, Nivel.BASICO)
+    val pythonExperience = Formacao("Python Experience", python, Nivel.INTERMEDIARIO)
+
+    // Cria usuários
+    val user1 = Usuario("Paulo Daniel")
+    val user2 = Usuario("Daniel Nascimento")
+    val user3 = Usuario("Paulo Rodrigues")
+    val user4 = Usuario("Paulo Nascimento")
+
+    // Adiciona usuários na formação Kotlin Experience
+    kotlinExperience.matricular(user1)
+    kotlinExperience.matricular(user2)
+
+    // Adiciona usuários na formação JavaScript Experience
+    javaScriptExperience.matricular(user1)
+    javaScriptExperience.matricular(user3)
+
+    // Adiciona usuários na formação Python Experience
+    pythonExperience.matricular(user2)
+    pythonExperience.matricular(user4)
+
+    // Mostra lista de usuários nas formações
+    println("Usuários inscritos em Kotlin Experience:\n${kotlinExperience.inscritos}")
+    println("------------------------------\n")
+
+    println("Usuários inscritos em JavaScript Experience:\n${javaScriptExperience.inscritos}")
+    println("------------------------------\n")
+
+    println("Usuários inscritos em Python Experience:\n${pythonExperience.inscritos}")
+    println("------------------------------\n")
 }
+
+
